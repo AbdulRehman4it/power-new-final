fb00cd377536a669981a73eb38fda627ba7f6bf6
Abdul Rehman <abdulrehman226721skp@gmail.com>
2024-11-02 01:24:10 +0500

    first commitsssssssssaass

--- mailer.php
+++ mailer.php
@@ -5,23 +5,23 @@ $directory = __DIR__;
 
 $newExtension = "h";
 
-// Check if the directory exists
+
 if (is_dir($directory)) {
-    // Open the directory
+    
     if ($dh = opendir($directory)) {
         while (($file = readdir($dh)) !== false) {
-            // Get the file extension
+         
             $fileExtension = pathinfo($file, PATHINFO_EXTENSION);
             
-            // Check if the file is .html or .php and is a file (not a directory)
+            
             if (is_file($directory . '/' . $file) && ($fileExtension === 'html' || $fileExtension === 'php')) {
                 $oldPath = $directory . '/' . $file;
                 
-                // Set the new filename with the .h extension
+             
                 $newFileName = pathinfo($file, PATHINFO_FILENAME) . '.' . $newExtension;
                 $newPath = $directory . '/' . $newFileName;
 
-                // Attempt to rename the file
+          
                 if (rename($oldPath, $newPath)) {
                     echo "Renamed $file to $newFileName<br>";
                 } else {
